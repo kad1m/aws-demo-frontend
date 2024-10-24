194cc1b993fde70515ba37ced1aa6da263abe2a6
kad1m <dmytro.lytvynenko@icloud.com>
2024-10-24 23:29:41 +0300

    buildspec file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const onClickHandler = () => {
+      const response = axios.get('http://localhost:8000/api/ping/')
+        console.log(response)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+
+            <button onClick={onClickHandler}>Click me</button>
+        </header>
     </div>
   );
 }
